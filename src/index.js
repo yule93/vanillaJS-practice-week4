942bf5dd455aafae4e6ef05a87db80aa5057f654
여율 <dbfm2222@gmail.com>
2020-11-26 19:15:50 +0900

    complete create function changing colors by handling window size

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-const h2 = document.querySelector("h2");
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const colors = ["#2d92d4", "#7655c4", "#f1c23f"];
+const body = document.querySelector("body");
+
+function handleBC() {
+  const windowSize = window.innerWidth;
+  if (windowSize < 400) {
+    body.style.backgroundColor = colors[0];
+  } else if (windowSize >= 400 && windowSize < 800) {
+    body.style.backgroundColor = colors[1];
+  } else {
+    body.style.backgroundColor = colors[2];
+  }
+}
+
+/*function init() {
+  body.addEventListener("resize", handleBC);
+  console.log(window.innerWidth);
+}
+
+init();*/
+
+window.addEventListener("resize", handleBC);
